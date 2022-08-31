e51ec1ea99b3215d6cb8e846a3efc8cf3195235d
bmitc <65685447+bmitc@users.noreply.github.com>
2022-08-31 01:08:22 -0400

    Replace chapter 1 and 2 scripts with notebooks

---
+++ CommonTypes.fsx
@@ -0,0 +1,7 @@
+/// Contains common types that are shared across the chapters of The Littler MLer
+module CommonTypes
+
+/// From Chapter 1
+type Num =
+    | Zero
+    | OneMoreThan of Num
---
+++ Utilities.fsx
@@ -0,0 +1,54 @@
+/// Helper functions mainly to help print out types in .NET Interactive notebooks
+module Utilities
+
+open Microsoft.FSharp.Quotations
+open Microsoft.FSharp.Quotations.Patterns
+open Microsoft.FSharp.Reflection
+open System.Text.RegularExpressions
+
+let private removeFSI typeString =
+    let fsiRemoved = Regex.Replace(typeString, "FSI_[0-9]+\+", "")
+    Regex.Replace(fsiRemoved, "\+_?[a-zA-Z]+", "")
+
+let private removeSystem typeString =
+    Regex.Replace(typeString, "System\.+", "")
+
+let private replaceObject typeString =
+    Regex.Replace(typeString, "Object", "'T")
+
+let private replaceGeneric typeString =
+    Regex.Replace(typeString, "\`[0-9]*\[.*\]", "<'T>")
+
+/// Helper function to return the type of a value in a .NET Interactive notebook as a string
+let getType value =
+    value.GetType().ToDisplayString()
+    |> removeFSI
+    |> removeSystem
+    |> replaceObject
+
+/// Helper function to return the type signature of a function in a .NET Interactive notebook as a string
+let getTypeSignature f =
+    let typeSignature =
+        let rec loop nested t =
+            if FSharpType.IsTuple t then
+                FSharpType.GetTupleElements t
+                |> Array.map (loop true)
+                |> String.concat " * "
+            elif FSharpType.IsFunction t then
+                let fs = if nested then sprintf "(%s -> %s)" else sprintf "%s -> %s"
+                let domain, range = FSharpType.GetFunctionElements t
+                fs (loop true domain) (loop false range)
+            else
+                t.FullName
+        loop false (f.GetType())
+        |> removeFSI
+        |> removeSystem
+        |> replaceGeneric
+        |> replaceObject
+    typeSignature
+
+/// Helper function to print a value with a label
+let printWithLabel label value = printfn "%s: %A" label value
+
+/// Helper function to return a string with a label
+let sprintWithLabel label value = sprintf "%s: %s" label value
